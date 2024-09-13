3470e0e206c4f5700f9927af0972b7d736d35488
Sivabalan Shanmugasiva <sivansbhfe@gmail.com>
2024-09-14 01:29:12 +0530

    All code except input preprocessing completed

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Using flask to make an api
 # import necessary libraries and functions
+from io import BytesIO
+
 from flask import (
     Flask,
     request,
@@ -9,18 +11,20 @@ from flask import (
     send_from_directory,
 )
 from flask_cors import CORS
-import networkx as nx
 import matplotlib
-import matplotlib.pyplot as plt
-from io import BytesIO
-import random
-import math
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-# Download necessary NLTK data (if you haven't already)
+# Download necessary NLTK datasets
 nltk.download('vader_lexicon')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # Run in background
 matplotlib.use("agg")
@@ -28,7 +32,6 @@ matplotlib.use("agg")
 # creating a Flask app
 app = Flask(__name__, template_folder="./", static_folder="./")
 CORS(app)
-
 
 # Render homepage
 @app.route("/")
@@ -43,13 +46,13 @@ def serve_static_files(filename):
 
 
 @app.route("/sentimentanalysis", methods=["POST"])
-def entityquery():
+def sentimentanalysis():
     details = request.data
     decoded = json.loads(details)
-    testing = eval(decoded)
-    print(testing)
+    rawtext = eval(decoded)
+    inputtext = preprocess_text(rawtext) # Get the input content pre-processed
     sia = SentimentIntensityAnalyzer()
-    sentiment_scores = sia.polarity_scores(testing)
+    sentiment_scores = sia.polarity_scores(inputtext)
 
     # Determine sentiment category based on compound score
     if sentiment_scores['compound'] >= 0.05:
@@ -59,38 +62,41 @@ def entityquery():
     else:
         sentiment = 'Neutral'
     return sentiment
-    # return sentiment, sentiment_scores
+
+@app.route("/sentimentscoreandvisual",methods=["POST"])
+def sentimentscoreandvisual():
+    details = request.data
+    decoded = json.loads(details)
+    rawtext1 = eval(decoded)
+    inputtext1 = preprocess_text(rawtext1) # Get the input content pre-processed
+    sia = SentimentIntensityAnalyzer()
+    sentiment_scores = sia.polarity_scores(inputtext1)
+    plt.figure(figsize=(4, 4))
+    plt.bar(sentiment_scores.keys(), sentiment_scores.values(), color='lightblue',width = 0.1)
+    plt.xlabel('Sentiment')
+    plt.ylabel('Scores')
+    plt.title('Actual scores from Sentiment Analyzer for reference:',fontsize = 8)
+    for i, v in enumerate(sentiment_scores.values()):
+        plt.text(i,v+0.02,str(v),ha='center')
+    img = BytesIO()  # file-like object for the image
+    plt.savefig(img)  # save the image to the stream
+    img.seek(0)  # writing moved the cursor to the end of the file, reset
+    plt.clf()  # clear pyplot
+    return send_file(img, mimetype="image/png")
 
 
-# @app.route("/addrelationship", methods=["POST"])
-# def addrelationship():
-#     details = request.data
-#     decoded = json.loads(details)
-#     testing = eval(decoded)
-#     G = nx.DiGraph()
-#     print("TESTING", len(testing))
-#     for i in range(len(testing)):
-#         G.add_node(testing[i]["fentity"], name=testing[i]["fentity"])
-#         G.add_node(testing[i]["sentity"], name=testing[i]["sentity"])
-#         # G.add_edge(testing[i]['fentity'], testing[i]['sentity'], start=testing[i]['fentity'], end=testing[i]['fentity'],relation=testing[i]['fentity']+' '+testing[i]['relationship']+' '+testing[i]['sentity'])
-#         G.add_edge(testing[i]["fentity"], testing[i]["sentity"])
-#     n = G.number_of_nodes()
-#     if n == 0:
-#         n = 1
-#     pos = nx.spring_layout(G, k=(8 / math.sqrt(n)))
-#     # pos = hierarchy_pos(G)
-#     nx.draw(G, pos, node_shape="s", node_size=1000)
-#     node_labels = nx.get_node_attributes(G, "name")
-#     nx.draw_networkx_labels(G, pos, labels=node_labels)
-#     edge_labels = nx.get_edge_attributes(G, "relation")
-#     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-#     img = BytesIO()  # file-like object for the image
-#     plt.savefig(img)  # save the image to the stream
-#     img.seek(0)  # writing moved the cursor to the end of the file, reset
-#     plt.clf()  # clear pyplot
-#     return send_file(img, mimetype="image/png")
+# Text preprocessing function
+def preprocess_text(text):
+      # Convert to lowercase
+      text = text.lower()
 
+      # Tokenize text
+      tokens = word_tokenize(text)
 
+      # Remove punctuation
+      tokens = [word for word in tokens if word.isalnum()]
+
+      return ' '.join(tokens)
 
 
 # driver function
--- script.js
+++ script.js
@@ -13,6 +13,12 @@ function changeContent(page) {
 function runsentimentanalysis() {
 	var textinput = document.getElementById('inputtextarea').value;
 	stringifiedlisttextarea = JSON.stringify(textinput);
+	const containertable2 = document.getElementById("repeatinput");
+	formatted2 = '<span>';
+	formatted2  += textinput;
+	formatted2  += '</span>';
+	containertable2.innerHTML = "";
+	containertable2.innerHTML = formatted2;
 			// a POST request
 	const response = fetch('http://127.0.0.1:5000/sentimentanalysis', {
 	method: 'POST',
@@ -20,13 +26,29 @@ function runsentimentanalysis() {
 	body: JSON.stringify(stringifiedlisttextarea)
 	}).then((response) => response.text())
   .then((response) => {
-    const datafetched = response
     const containertable = document.getElementById("resultarea");
-	formatted = '<span>';
-	formatted  += datafetched;
+	containertable.innerHTML=""
+	formatted = '<span id="sentimentoutput">';
+	formatted  += response;
 	formatted  += '</span>';
-   	containertable.innerHTML = formatted;
+   	containertable.innerHTML += "The sentiment category for the above text is: " + formatted;
+	const containertable3 = document.getElementById("sentimentoutput");
+	containertable3.setAttribute("Value",response);
 	  });
+			// a POST request
+	const responseimg = fetch('http://127.0.0.1:5000/sentimentscoreandvisual', {
+	method: 'POST',
+	contentType: 'application/json',
+	body: JSON.stringify(stringifiedlisttextarea)
+	}).then((responseimg) => responseimg.blob())
+	.then((blob) => {
+	const imageUrl = URL.createObjectURL(blob);
+	const imageElement = document.createElement("img");
+	imageElement.src = imageUrl;
+	const container = document.getElementById("graphdisplayarea");
+	container.innerHTML=""
+	container.appendChild(imageElement);
+		});
 }
 
 
@@ -39,6 +61,12 @@ function readSingleFile (evt) {
 	var contents = e.target.result;
 	var lines = contents
 	const containertable = document.getElementById("resultarea");
+	const containertable2 = document.getElementById("repeatinput");
+	formatted2 = '<span>';
+	formatted2  += contents;
+	formatted2  += '</span>';
+	containertable2.innerHTML = "";
+	containertable2.innerHTML = formatted2;
 	// formatted = '<span>';
 	// formatted  += lines;
 	// formatted  += '</span>';
@@ -52,14 +80,31 @@ function readSingleFile (evt) {
 	body: JSON.stringify(stringifiedlistfileupload)
 	}).then((response) => response.text())
   .then((response) => {
-    const datafetched = response
     const containertable = document.getElementById("resultarea");
-	formatted = '<span>';
-	formatted  += datafetched;
+	containertable.innerHTML=""
+	formatted = '<span id="sentimentoutput">';
+	formatted  += response;
 	formatted  += '</span>';
-   	containertable.innerHTML = formatted;
+	containertable.innerHTML += "The sentiment category for the above text is: " + formatted;
+	const containertable3 = document.getElementById("sentimentoutput");
+	containertable3.setAttribute("Value",response);
+
 	  });
 
+			// a POST request
+	const responseimg = fetch('http://127.0.0.1:5000/sentimentscoreandvisual', {
+	method: 'POST',
+	contentType: 'application/json',
+	body: JSON.stringify(stringifiedlistfileupload)
+	}).then((responseimg) => responseimg.blob())
+	.then((blob) => {
+	const imageUrl = URL.createObjectURL(blob);
+	const imageElement = document.createElement("img");
+	imageElement.src = imageUrl;
+	const container = document.getElementById("graphdisplayarea");
+	container.innerHTML=""
+	container.appendChild(imageElement);
+		});
 	}
 	r.readAsText(f);
 	document.getElementById("fileinput").value=""
@@ -67,6 +112,8 @@ function readSingleFile (evt) {
 	alert("Failed to load file");
 	}
 }
+
+
 
 function refresh(){
 	location.reload();
